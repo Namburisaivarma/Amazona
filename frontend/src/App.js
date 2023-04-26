ee5f0432b007feab3d3e9b5a62bb1b99f43ed25a
sai varma <saivarma.shanmukha@gmail.com>
2023-04-26 12:59:56 +0530

    headercss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
+import data from './data';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
